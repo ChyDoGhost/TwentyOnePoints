c3cbaa48c5271b69b49b3f5f5479f733b8c206e1
Vladislav Zhukov <vzhukov147@gmail.com>
2022-07-08 14:17:23 +0300

    First commit
    The first commit of the project is TwentyOnePoints

---
+++ TwentyOnePoints/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Text;
+using System.Drawing;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace TwentyOnePoints
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
